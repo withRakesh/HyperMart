8ecd4a178188309c05093ac9018010b2d8536c17
withRakesh <rakesh2003cs@gmail.com>
2025-11-22 10:36:05 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const  productRoutes = require('../server/routes/productRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://hypermartapp.netlify.app", "http://localhost:5173"],
+  credentials: true
+}));
 
 app.use(express.json());
 
