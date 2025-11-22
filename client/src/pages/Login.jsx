b1b9519bbd63f5b71fcebe013a138a7fee42db12
withRakesh <rakesh2003cs@gmail.com>
2025-11-22 10:17:03 +0530

    modified

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
       }
 
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://hypermart-ysoc.onrender.com/api/auth/login",
         user
       );
       console.log(res.data);
